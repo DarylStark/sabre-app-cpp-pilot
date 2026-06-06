cd3f7680171f57c5e5d859c2d8da8ebf1183d95a
Daryl Stark <daryl@dstark.nl>
2026-06-06 19:03:51 +0000

    chore: Remove dead code.

--- src/pilot/main.cpp
+++ src/pilot/main.cpp
@@ -66,26 +66,6 @@ int main(int argc, char *argv[])
 
             sabrePilot.addDevice(*name, config, *library, *entryPoint);
         }
-
-        //----------------------------------------------------------------------
-
-        // sabre_pilot::Project project;
-        // project.devices.emplace_back(sabre_pilot::ProjectDevice{
-        //     .config = {.maxGpios = 1, .upperboundUart = 3},
-        //     .name = "Device01",
-        //     .library = "../app_example_app/libapp_example_app.so",
-        //     .entryPoint = "startDev1"});
-        // project.devices.emplace_back(sabre_pilot::ProjectDevice{
-        //     .config = {.maxGpios = 1, .upperboundUart = 3},
-        //     .name = "Device02",
-        //     .library = "../app_example_app/libapp_example_app.so",
-        //     .entryPoint = "startDev2"});
-
-        // for (const auto &device : project.devices)
-        // {
-        //     sabrePilot.addDevice(device.name, device.config, device.library,
-        //                          device.entryPoint);
-        // }
     }
 
     sabrePilot.run();
