fa71880a6255d27a7f8103609a4c90d2200337e8
Daryl Stark <daryl@dstark.nl>
2026-06-22 21:10:41 +0000

    feat: Started with Conan.
    GLFW3 and ImGui are now installed via Conan. The documentation is too
    small but good enough. I also have to add TOML++ and Google Test to
    Conan, but I will do that at a later time.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Sabre Pilot
 
 Readme will follow later.
+
+## Setup
+
+1.  Install Conan dependencies:
+
+    ```bash
+    conan install . -s build_type=Debug --build=missing
+    ```
+
+2.  Normal CMake stuff.
--- src/pilot_ui_imgui/imgui.cpp
+++ src/pilot_ui_imgui/imgui.cpp
@@ -1,7 +1,7 @@
 #include "imgui.hpp"
 #include <GLFW/glfw3.h>
-#include <backends/imgui_impl_glfw.h>
-#include <backends/imgui_impl_opengl3.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include <iostream>
 
 namespace sabre_ui_imgui
