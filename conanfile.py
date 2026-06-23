1fbd310d415238d36b476cd99d1474c5b5c60b5d
Daryl Stark <daryl@dstark.nl>
2026-06-23 12:18:37 +0000

    feat: Add conanfile.py for package management.

---
+++ conanfile.py
@@ -0,0 +1,23 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout, CMakeDeps, CMakeToolchain
+
+
+class SabreAppPilotConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    
+    requires = (
+        "glfw/3.4",
+        "imgui/1.92.8-docking",
+    )
+
+    default_options = {
+        "imgui/*:with_glfw": True,
+        "imgui/*:with_opengl3": True,
+    }
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        CMakeDeps(self).generate()
+        CMakeToolchain(self).generate()
