b8614d548814b88d97ddb023590c82fc656522fb
Daryl Stark <daryl@dstark.nl>
2026-06-23 19:21:56 +0000

    ci: Skip installation of `glfw`.

--- conanfile.py
+++ conanfile.py
@@ -1,3 +1,4 @@
+import os
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout, CMakeDeps, CMakeToolchain
 
@@ -5,17 +6,25 @@ from conan.tools.cmake import cmake_layout, CMakeDeps, CMakeToolchain
 class SabreAppPilotConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     
-    requires = (
-        "glfw/3.4",
-        "imgui/1.92.8-docking",
-        "gtest/1.17.0",
-        "tomlplusplus/3.4.0"
-    )
 
     default_options = {
         "imgui/*:with_glfw": True,
         "imgui/*:with_opengl3": True,
     }
+    
+    def requirements(self):
+        skips = os.getenv("CONAN_SKIPS", default='').split()
+        requires = (
+            "glfw/3.4",
+            "imgui/1.92.8-docking",
+            "gtest/1.17.0",
+            "tomlplusplus/3.4.0"
+        )
+        
+        for requirement in requires:
+            if requirement.split('/')[0] in skips:
+                continue
+            self.requires(requirement)
 
     def layout(self):
         cmake_layout(self)
